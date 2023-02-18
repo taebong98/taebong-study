8822569ed1827b238614a57dc1cee48ff5a35622
TaeHeyeon_Kim <thk98k@gmail.com>
2023-02-19 01:09:34 +0900

    Feat: LogController 구현
    간단한 로그를 출력하는 컨트롤러

---
+++ src/main/java/com/thk98k/study/log/LogController.java
@@ -0,0 +1,18 @@
+package com.thk98k.study.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogController {
+    @GetMapping("/log")
+    public void log() {
+        log.trace("trace message");
+        log.debug("debug message");
+        log.info("info message"); // default
+        log.warn("warn message");
+        log.error("error message");
+    }
+}
